17dd9e9016034579dbfea2106e58c0ea7506b56e
YourtionGuo <yourtion@gmail.com>
2016-03-27 20:33:50 +0800

    Init Project

---
+++ HTTPDNS/HTTPDNS.h
@@ -0,0 +1,19 @@
+//
+//  HTTPDNS.h
+//  HTTPDNS
+//
+//  Created by YourtionGuo on 3/27/16.
+//  Copyright © 2016 Yourtion. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for HTTPDNS.
+FOUNDATION_EXPORT double HTTPDNSVersionNumber;
+
+//! Project version string for HTTPDNS.
+FOUNDATION_EXPORT const unsigned char HTTPDNSVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <HTTPDNS/PublicHeader.h>
+
+
--- README.md
+++ README.md
@@ -1,2 +1,13 @@
 # HTTPDNS-OC
+
 HTTPDNS 库 Objdctive-C 实现（使用DNSPod）
+
+## TODO
+
+- [ ] 实现 DNSPod 免费版功能
+- [ ] 实现 DNSPod 企业版功能（认证接入）
+- [ ] 实现AliYun HTTPDNS
+- [ ] 自动转换 `URLRequest`
+- [ ] 提供同步获取方法
+- [ ] 允许在初始化时关闭缓存
+- [ ] 提供清除缓存方法
