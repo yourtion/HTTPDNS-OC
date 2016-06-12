4162a850e632c973a1800802029f77880219ecdb
YourtionGuo <yourtion@gmail.com>
2016-06-12 11:40:13 +0800

    HTTPDNSBase

---
+++ HTTPDNS/HTTPDNSBase.h
@@ -0,0 +1,33 @@
+//
+//  HTTPDNSBase.h
+//  HTTPDNS
+//
+//  Created by YourtionGuo on 6/12/16.
+//  Copyright © 2016 Yourtion. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import "HTTPDNSRecord.h"
+
+typedef enum : NSUInteger {
+    HTTPDNSProviderDNSPod,
+    HTTPDNSProviderDNSPodPro,
+    HTTPDNSProviderAliYun,
+} HTTPDNSProvider;
+
+const static NSString *kHTTPDNS_DNSPOD_SERVER_ADDRESS = @"http://119.29.29.29/";
+
+@protocol HTTPDNSBaseProtocol
+
+@required
+- (HTTPDNSRecord *)parseResult:(NSData *)data;
+- (NSString *)getRequestString:(NSString *)domain;
+
+@end
+
+@interface HTTPDNSBase : NSObject  <HTTPDNSBaseProtocol>
+
+- (void)requsetRecord:(NSString *)domain callback:(void (HTTPDNSRecord *record))callback;
+- (HTTPDNSRecord *)requsetRecordSync:(NSString *)domain;
+
+@end
---
+++ HTTPDNS/HTTPDNSBase.m
@@ -0,0 +1,39 @@
+//
+//  HTTPDNSBase.m
+//  HTTPDNS
+//
+//  Created by YourtionGuo on 6/12/16.
+//  Copyright © 2016 Yourtion. All rights reserved.
+//
+
+#import "HTTPDNSBase.h"
+
+@implementation HTTPDNSBase
+
+-(NSString *) getRequestString:(NSString *)domain {
+    return @"";
+}
+
+-(HTTPDNSRecord *)parseResult:(NSData *)data {
+    return nil;
+}
+
+- (void)requsetRecord:(NSString *)domain callback:(void (HTTPDNSRecord *record))callback {
+    NSString *urlString = [self getRequestString:domain];
+    NSURL *url = [NSURL URLWithString:urlString];
+    
+    [[NSURLSession sharedSession] dataTaskWithURL:url completionHandler:^(NSData * _Nullable data, NSURLResponse * _Nullable response, NSError * _Nullable error) {
+        HTTPDNSRecord *res = [self parseResult:data];
+        callback(res);
+    }];
+}
+
+-(HTTPDNSRecord *)requsetRecordSync:(NSString *)domain {
+    NSString *urlString = [self getRequestString:domain];
+    NSURL *url = [NSURL URLWithString:urlString];
+    NSData *data = [NSData dataWithContentsOfURL:url];
+    HTTPDNSRecord *res = [self parseResult:data];
+    return res;
+}
+
+@end
