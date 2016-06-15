58ea17e706c843dd9f6eb3180be0ee2850e5eecb
YourtionGuo <yourtion@gmail.com>
2016-06-16 00:21:17 +0800

    update mac support

--- HTTPDNS/HTTPDNSUtil.m
+++ HTTPDNS/HTTPDNSUtil.m
@@ -9,18 +9,30 @@
 #import "HTTPDNSUtil.h"
 
 #if TARGET_OS_IPHONE
-    #import <UIKit/UIKit.h>
+#import <UIKit/UIKit.h>
 #endif
 
 @implementation HTTPDNSUtil
 
-+ (BOOL)isHTTPEnable {
-    #if TARGET_OS_IPHONE
++ (BOOL)needTransportSecurity {
+#if TARGET_OS_IPHONE
     if([[[UIDevice currentDevice] systemVersion] compare:@"9.0" options:NSNumericSearch] != NSOrderedAscending){
+        return YES;
+    }
+#elif TARGET_OS_MAC
+    NSDictionary *systemVersionDictionary = [NSDictionary dictionaryWithContentsOfFile:@"/System/Library/CoreServices/SystemVersion.plist"];
+    if([[systemVersionDictionary objectForKey:@"ProductVersion"] compare:@"10.11" options:NSNumericSearch] != NSOrderedAscending){
+        return YES;
+    }
+#endif
+    return NO;
+}
+
++ (BOOL)isHTTPEnable {
+    if([HTTPDNSUtil needTransportSecurity]){
         NSDictionary *infoDict = [[NSBundle mainBundle] infoDictionary];
         return [[[infoDict objectForKey:@"NSAppTransportSecurity"] objectForKey:@"NSAllowsArbitraryLoads"] boolValue];
     }
-    #endif
     return YES;
 }
 
