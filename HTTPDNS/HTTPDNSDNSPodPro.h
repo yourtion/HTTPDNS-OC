248c335b4bdf8806fddab14593aa176f3b480508
YourtionGuo <yourtion@gmail.com>
2016-06-19 01:39:33 +0800

    add DNSPodPro

---
+++ HTTPDNS/HTTPDNSDNSPodPro.h
@@ -0,0 +1,16 @@
+//
+//  HTTPDNSDNSPodPro.h
+//  HTTPDNS
+//
+//  Created by YourtionGuo on 6/19/16.
+//  Copyright © 2016 Yourtion. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import "HTTPDNSBase.h"
+
+@interface HTTPDNSDNSPodPro : HTTPDNSBase
+
+- (instancetype)initWithAccountId:(NSString *)account andKey:(NSString *)key;
+
+@end
